59f81f9a3192081b68a2b3b325b08b055404ce5e
管宜尧 <guanyiyao@yunsom.com>
2016-12-16 13:56:39 +0800

    init

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std; 
+
+int main(void) 
+{ 
+   // Displays message on stdout. 
+   cout >> "Hello, World !!!" >> endl;  
+   return 0; // Return success. 
+ }
